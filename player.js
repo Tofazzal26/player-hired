08b8802c71e28275da782ec1b0c49c635270afcc
Tofazzal26 <afranislamabir6789@gmail.com>
2024-02-21 16:37:53 +0600

    all validation finish

--- player.js
+++ player.js
@@ -5,6 +5,10 @@ const addToCart = document.querySelectorAll('.add-btn');
 for( let i = 0; i < addToCart.length; i++) {
     const element = addToCart[i];
     element.addEventListener('click', function(event) {
+
+        event.target.setAttribute('disabled', true);
+        
+
         const playerName = event.target.parentNode.childNodes[3].innerText;
         const price = event.target.parentNode.childNodes[5].childNodes[1].innerText;
         const category = event.target.parentNode.childNodes[7].childNodes[1].innerText;
@@ -23,18 +27,75 @@ for( let i = 0; i < addToCart.length; i++) {
         div.appendChild(p3);
         selectedPlayer.appendChild(div);
 
+        if( valueConvert('cart') + 1 > 6) {
+            alert('You will be 6 player select');
+            return;
+        }
+        event.target.parentNode.style.background = 'orange';
+
+        const cart = valueConvert('cart');
+        document.getElementById('cart').innerText = cart + 1;
+
+        const left = valueConvert('left');
+        document.getElementById('left').innerText = left - 1;
+
+
+        
         totalCost(price);
+        updateGrandTotal ();
+        budgetValue(price);
 
     })
 }
 
+// function CartandLeft() {
+//     let cart = valueConvert('cart');
+//     cart = cart + 1
+//     document.getElementById('cart').innerText = cart;
+//     let left = valueConvert('left');
 
+//     if( left > 0) {
+//         left = left - 1
+//         document.getElementById('left').innerText = left;
+//         return;
+//     }
+// }
+
+
+function budgetValue(price) {
+    const priceCurrent = parseInt(price)
+    const budget = valueConvert('budget');
+    document.getElementById('budget').innerText =  budget - priceCurrent;
+}
 
 
 function totalCost(price) {
     const totalCost = valueConvert('total-cost');
     const playerValue = parseInt(price);
     document.getElementById('total-cost').innerText = totalCost + playerValue;
+}
+
+
+function updateGrandTotal (input) {
+    if ( input == undefined) {
+        const grandTotal = valueConvert('total-cost');
+        document.getElementById('grand-total').innerText = grandTotal;
+    } else {
+        const couponCode = document.getElementById('coupon-code').value;
+        if( couponCode == "love420") {
+            const totalCost = valueConvert('total-cost');
+            const discount = totalCost * .2;
+            document.getElementById('grand-total').innerText = totalCost - discount;
+            const couponInput = document.getElementById('input-section');
+            couponInput.style.display = "none";
+            
+        } else if( couponCode == "") {
+            alert('Please inter a valid coupon code')
+        }
+         else {
+            alert('coupon code is not match !');
+        }
+    }
 }
 
 
