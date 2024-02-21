47999fc25443c0b197e9fc54c65527297c798f3a
Tofazzal26 <afranislamabir6789@gmail.com>
2024-02-21 14:28:23 +0600

    total validation complete

--- player.js
+++ player.js
@@ -1,5 +1,47 @@
 
 
+const addToCart = document.querySelectorAll('.add-btn');
 
+for( let i = 0; i < addToCart.length; i++) {
+    const element = addToCart[i];
+    element.addEventListener('click', function(event) {
+        const playerName = event.target.parentNode.childNodes[3].innerText;
+        const price = event.target.parentNode.childNodes[5].childNodes[1].innerText;
+        const category = event.target.parentNode.childNodes[7].childNodes[1].innerText;
+        
+        const selectedPlayer = document.getElementById('selected-players-container');
+        const div = document.createElement('div');
+        const p1 = document.createElement('p');
+        const p2 = document.createElement('p');
+        const p3 = document.createElement('p');
+        p1.innerText = playerName;
+        p2.innerText = price;
+        p3.innerText = category;
+        div.classList.add('selected-players');
+        div.appendChild(p1);
+        div.appendChild(p2);
+        div.appendChild(p3);
+        selectedPlayer.appendChild(div);
+
+        totalCost(price);
+
+    })
+}
+
+
+
+
+function totalCost(price) {
+    const totalCost = valueConvert('total-cost');
+    const playerValue = parseInt(price);
+    document.getElementById('total-cost').innerText = totalCost + playerValue;
+}
+
+
+function valueConvert(id) {
+    const element = document.getElementById(id).innerText;
+    const convertedValue = parseInt(element);
+    return convertedValue;
+}
 
 
